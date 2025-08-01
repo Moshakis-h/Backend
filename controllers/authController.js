e14ee3dc539fbda768651a21052fb9afdceddd41
Moshakis-h <moshakisloat@gmail.com>
2025-08-01 19:50:19 +0100

    hg

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,66 +1,3 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const createToken = require("../utils/createToken");
-
-const register = async (req, res) => {
-  const { name, email, phone, password, role } = req.body;
-
-  if (!name || !email || !phone || !password)
-    return res.status(400).json({ message: "Please fill in all fields. " });
-
-  try {
-    const emailExists = await User.findOne({ email });
-    const phoneExists = await User.findOne({ phone });
-
-    if (emailExists)
-      return res.status(409).json({ message: "There is an error in the registration information." });
-
-    if (phoneExists)
-      return res.status(409).json({ message: "There is an error in the registration information." });
-
-    const newUser = new User({ name, email, phone, password, role });
-    await newUser.save();
-
-    res.status(201).json({ message: "Account created" });
-  } catch (err) {
-    res.status(500).json({ message: "A server error occurred." });
-  }
-};
-
-const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ 
-      email: { $regex: new RegExp(`^${email}$`, 'i') } 
-    });
-    
-    if (!user) return res.status(401).json({ message: "Invalid login data" });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(401).json({ message: "Invalid login data" });
-
-    const token = createToken(user);
-
-    res.json({
-      message: "Login successful",
-      token,
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: user.role
-      }
-    });
-  } catch (err) {
-    res.status(500).json({ message: "Server error" });
-  }
-};
-
-const logout = (req, res) => {
-  res.json({ message: "You are logged out" });
-};
-
 const verify = async (req, res) => {
   try {
     if (!req.user) {
@@ -84,17 +21,7 @@ const verify = async (req, res) => {
       }
     });
   } catch (err) {
+    console.error("Verify error:", err);
     res.status(200).json({ isAuthenticated: false });
   }
 };
-
-const admin = async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: "حدث خطأ في السيرفر" });
-  }
-};
-
-module.exports = { register, login, logout, verify, admin };
